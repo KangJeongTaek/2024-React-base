8bb114dd4c9c82452d1455892e5bc2a69f250e8c
KangJeongTaek <dfgddjd@naver.com>
2024-06-20 16:28:12 +0900

    변경

--- hello-react/hello-react/src/App.js
+++ hello-react/hello-react/src/App.js
@@ -1,14 +1,7 @@
 
 function App() {
-  const name = '리액트1';
-  return (
-    <>
-    {name ==='리액트' ? (<h1>리액트입니다.</h1>)
-    : (
-      <h2>리액트가 아닙니다.</h2>
-    )}
-    </>
-  );
+  const name= 'react';
+  return <div>{name === '리액트' ?<h1>리액트입니다.</h1> : null}</div>;
 }
 
 export default App;
