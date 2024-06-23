170a1f6e1f4509bc19f0b8ad30708c17534141c9
KangJeongTaek <dfgddjd@naver.com>
2024-06-23 13:02:37 +0900

    잠시 휴식

--- hello-react/hello-react/src/App.js
+++ hello-react/hello-react/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import IterationSample from './iteration/iterationSample'
+import React, { Component } from 'react';
+import LifleCycleSample from './lifeCycleSample/LifleCycleSample';
+import ErrorBoundary from './errorBoundary/ErrorBoundary';
 
-const App = () => {
-  return (
-    <div>
-      <IterationSample/>  
-    </div>
-  );
-};
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () =>{
+    this.setState({
+      color:getRandomColor()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifleCycleSample color={this.state.color}/>
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
