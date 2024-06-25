30824e48cc72004a0e90bc96bbdd71015fa0dd9b
KangJeongTaek <dfgddjd@naver.com>
2024-06-25 20:00:26 +0900

    프로젝트 생성

---
+++ projects/styling-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from "./Sass/SassComponent";
+
+function App() {
+  return (
+    <div>
+      <SassComponent/>
+    </div>
+  );
+}
+
+export default App;
