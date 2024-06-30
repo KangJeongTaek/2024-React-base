29e9a6e3f4dd9b393c4db4da66ca77abde271e12
KangJeongTaek <dfgddjd@naver.com>
2024-06-30 18:06:39 +0900

    6월 30일

--- projects/todo-app/src/App.js
+++ projects/todo-app/src/App.js
@@ -2,30 +2,69 @@ import TodoTemplate from './componenets/TodoTemplate';
 import './App.css';
 import TodoInsert from './componenets/TodoInsert';
 import TodoList from './componenets/TodoList';
-import { useState } from 'react';
+import { useCallback, useReducer, useRef, useState } from 'react';
+
+function createBulkTodos(){
+  const array = [];
+  for (let i = 1; i<=2500; i++){
+    array.push({
+      id:i,
+      text : `할 일 ${i}`,
+      checked:false,
+    })
+  }
+  return array;
+}
+function todoReducer(todos,action){
+  switch (action.type) {
+    case 'INSERT':
+      return todos.conat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? {...todo,checked:!todo.checked} : todo,
+      )
+    default:
+      return todos;
+  }
+}
 
 function App() {
-  const [todos,setTodos] = useState([
-    {
-      id :1,
-      text:"react의 기초 알아보기",
-      checked:true,
+  const [todos,dispatch] = useReducer(todoReducer,undefined,createBulkTodos);
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(2501);
+  const OnInsert = useCallback(
+    text =>{
+      const todo = {
+        id: nextId.current,
+        text,
+        checked:false,
+      };
+      dispatch({type:'INSERT',todo});
+      nextId.current += 1;
     },
-    {
-      id:2,
-      text:'컴포넌트 스타일링 해보기',
-      checked:true,
+    [],
+  );
+
+  const onRemove = useCallback(
+    id =>{
+      dispatch({type:'REMOVE',id});
     },
-    {
-      id:3,
-      text:'일정 관리 앱 만들기',
-      checked:false,
-    }
-  ]);
+    [],
+  );
+
+  const onToggle = useCallback(
+    id =>{
+      dispatch({type:'TOGGLE',id});
+     }, [],
+  )
+
   return (
     <TodoTemplate>
-      <TodoInsert/>
-      <TodoList todos={todos}/>
+      <TodoInsert onInsert={OnInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   );
 }
--- projects/todo-app/src/componenets/TodoList.js
+++ projects/todo-app/src/componenets/TodoList.js
@@ -3,11 +3,11 @@ import './TodoList.scss';
 
 import React from 'react';
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos,onRemove, onToggle}) => {
     return (
         <div className='TodoList'>
             {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} />
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
             ))}
         </div>
     );
