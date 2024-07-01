95a842d3984fc87bb27cf760da9841648e182d9c
KangJeongTaek <dfgddjd@naver.com>
2024-07-01 10:48:45 +0900

    2024 7 1 오전 잠깐11장까지

--- projects/todo-app/src/componenets/TodoListItem.js
+++ projects/todo-app/src/componenets/TodoListItem.js
@@ -1,18 +1,19 @@
+import cn from 'classnames';
 import {
-    MdCheckBoxOutlineBlank,
     MdCheckBox,
+    MdCheckBoxOutlineBlank,
     MdRemoveCircleOutline,
 } from 'react-icons/md';
-import cn from 'classnames';
 import './TodoListItem.scss';
 
 import React from 'react';
 
-const TodoListItem = ({todo,onRemove,onToggle}) => {
+const TodoListItem = ({todo,onRemove,onToggle,style}) => {
     const {id,text,checked} = todo;
 
     
     return (
+        <div className='TodoListItem-virtualized' style={style}>
         <div className='TodoListItem'>
             <div className={cn('checkbox',{checked})} onClick={ () =>onToggle(id)}>
                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
@@ -21,6 +22,7 @@ const TodoListItem = ({todo,onRemove,onToggle}) => {
             <div className='remove' onClick={ () => onRemove(id)}>
                 <MdRemoveCircleOutline/>
             </div>
+        </div>
         </div>
     );
 };
