0008ffe697808117763826da61c60c380a164d7c
KangJeongTaek <dfgddjd@naver.com>
2024-07-08 16:13:14 +0900

    7월 8일 마무리

--- projects/react-redux-tutorial/src/index.js
+++ projects/react-redux-tutorial/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import rootReducer from './modules';
 
+const store = createStore(rootReducer,devToolsEnhancer());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
